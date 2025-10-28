887b54b7d4bac55f32354acd40d1876cf0ea7ae0
naohello117 <221256931+naohello117@users.noreply.github.com>
2025-10-28 17:26:08 +0900

    index.jsからhttpTrigger1.jsに移植

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -1,13 +1,37 @@
 const { app } = require('@azure/functions');
 
+// --- [index.js から移植した関数] ---
+function get_time(){
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1; // 0-basedなので +1
+    var date = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    return (
+        year + "/" + 
+        month + "/" +
+        date + " " +
+        hour + ":" +
+        minute
+    );
+}
+// ------------------------------------
+
 app.http('httpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+        // 時刻を取得
+        const currentTime = get_time();
+        
+        // JSON形式でレスポンスを返す
+        return {
+            status: 200, // ステータスコード200 OK
+            // レスポンスのJSONオブジェクトを定義
+            jsonBody: {
+                time: currentTime
+            }
+        };
     }
 });
